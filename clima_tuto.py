2b59bb63d25a7f412c97f5d496a5a784e201ccc9
souffle <vsr.zhao@gmail.com>
2021-02-22 19:32:28 +0100

    Draft commit for pull creation

--- clima_tuto.py
+++ clima_tuto.py
@@ -5,3 +5,6 @@ df = pd.read_csv('jena_climate_2009_2016.csv')
 time = pd.to_datetime(df.pop('Date Time'), format='%d.%m.%Y %H:%M:%S')
 series = df['T (degC)'][5::6]
 series.index = time[5::6]
+
+
+print('To be commited to GIT')
